2ee60a4611e82ee7f7ea07dd9315bd7ff61b9001
Vanshaj Chowdhary <vchowdhary@c-73-4-79-54.hsd1.ct.comcast.net>
2018-08-17 22:48:01 -0400

    Add very basic foundation of game engine

---
+++ game/game.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python3
+import os, sys
+import pygame
+from pygame.locals import *
+from car import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((468, 60))
+    pygame.display.set_caption('Monkey Fever')
+    pygame.mouse.set_visible(0)
+
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((250, 250, 250))
+
+    if pygame.font:
+        font = pygame.font.Font(None, 36)
+        text = font.render("Pummel The Chimp, And Win $$$", 1, (10, 10, 10))
+        textpos = text.get_rect(centerx=background.get_width() / 2)
+        background.blit(text, textpos)
+
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+
+    fist = Fist()
+    allsprites = pygame.sprite.RenderPlain((fist))
+    clock = pygame.time.Clock()
+
+    while 1:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                return
+            elif event.type == MOUSEBUTTONDOWN:
+                fist.punch()
+            elif event.type == MOUSEBUTTONUP:
+                fist.unpunch()
+
+        allsprites.update()
+
+        # Draw Everything
+        screen.blit(background, (0, 0))
+        allsprites.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
